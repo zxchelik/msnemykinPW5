84b25fa48eb771dc769d308d523f1faa710f04b9
zxcdebil MB <mishany1105@gmail.com>
2025-03-24 13:31:35 +0300

    Some fixes

---
+++ msnemykinPW5/Modules/News/Cell/ArticleCellDelegate.swift
@@ -0,0 +1,10 @@
+//
+//  ArticleCellProtocol.swift
+//  msnemykinPW5
+//
+//  Created by Михаил Немыкин on 24.03.2025.
+//
+
+protocol ArticleCellDelegate: AnyObject {
+    func shareButtonTapped(in cell: ArticleCell)
+}
